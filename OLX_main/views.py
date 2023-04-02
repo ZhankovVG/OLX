fea80d640024eabcf008ee5cff08b73e1fd41b24
Vitaliy <sprinter-100m@ukr.net>
2023-04-02 18:29:29 +0300

    start project

--- OLX_main/views.py
+++ OLX_main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+
+class ProductView(ListView):
+    model = Product
+    queryset = Product.objects.filter(draft = False)
