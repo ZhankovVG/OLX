a028647fd470f0789a6968ac11ef58abc21b0a28
Vitaliy <sprinter-100m@ukr.net>
2023-04-02 13:54:49 +0300

    start project

---
+++ OLX_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OlxMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'OLX_main'
---
+++ OLX_main/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+
+class User(models.Model):
+    # Пользователь
+    name = models.CharField('Имя', max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class Category(models.Model):
+    # Категории
+    name = models.CharField('Название', max_length=70)
+    url = models.SlugField(max_length=120, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    # Разные товары
+    title = models.CharField('Название', max_length=70)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    poster = models.ImageField('Постер', upload_to='user_product/')
+    draft = models.BooleanField('В наличии', default=False)
+    url = models.SlugField(max_length=150, unique=True)
+    category = models.ForeignKey(
+        Category, verbose_name='Категории', on_delete=models.SET_NULL, null=True)
+    users = models.ManyToManyField(User, verbose_name='Пользователи', related_name='product_user')
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+class Comments(models.Model):
+    # Коментарий
+    name = models.CharField('Имя', max_length=150)
+    text = models.TextField('Коментарий', max_length=5000)
+    email = models.EmailField()
+    parent = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Родитель')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукты')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
